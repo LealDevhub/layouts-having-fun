6c4fd964ab834584ab2f40d301b16855f2f1f7c8
lealdevhub <fabriciopereiraleal2021@gmail.com>
2025-04-18 08:53:42 -0300

    Responsivo

--- dashboard/scripts.js
+++ dashboard/scripts.js
@@ -7,6 +7,9 @@ const settings = document.querySelector("#settings")
 const active = document.querySelector(".active")
 const sidebar = document.querySelector(".sidebar")
 
+const menu_sidebar = document.querySelector("#menu-sidebar")
+
+
 search.addEventListener("click", () => {
     active.style.top = "0"
 })
@@ -15,24 +18,20 @@ grid.addEventListener("click", () => {
     active.style.top = "54px"
 })
 
-pet.addEventListener("click", () => {
+user.addEventListener("click", () => {
     active.style.top = "108px"
 })
 
-user.addEventListener("click", () => {
-    active.style.top = "162px"
-})
-
-vet.addEventListener("click", () => {
-    active.style.top = "216px"
-})
-
 settings.addEventListener("click", () => {
-    active.style.top = "270px"
+    active.style.top = "162px"
 })
 
 const menu = document.querySelector("#menu")
 
 menu.addEventListener("click", () => {
+    sidebar.classList.toggle("show")
+})
+
+menu_sidebar.addEventListener("click" , () => {
     sidebar.classList.toggle("show")
 })
